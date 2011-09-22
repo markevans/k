522b291345783f8d1965df7ecd45b465fbd5c691
Mark Evans <mark@new-bamboo.co.uk>
2011-09-22 17:29:51 +0100

    Added view stuff

---
+++ src/view.js
@@ -0,0 +1,66 @@
+var View = Class('View', function(klass){
+  
+  this
+
+    .onInit(function(el, model){
+      var self = this
+      this.elem = this._elemFor(el)
+      this.model = model
+      
+      this.klass.onDOMSubscriptions.forEach(function(sub){
+        self._delegate(sub.selector, sub.event, function(){
+          var args
+          if(sub.argsMap){
+            args = sub.argsMap.apply(sub, arguments)
+            if(!Array.isArray(args)) args = [args]
+          } else {
+            args = arguments
+          }
+          model[sub.method].apply(model, args)
+        })
+      })
+      
+      this.klass.onModelSubscriptions.forEach(function(sub){
+        model.on(sub.event, function(){
+          self[sub.method].apply(self, arguments)
+        })
+      })
+
+    })
+  
+    .include({
+      _elemFor: function(el){
+        return jQuery(el)[0]
+      },
+      _delegate: function(selector, event, callback){
+        jQuery(this.elem).delegate(selector, event, callback)
+      }
+    })
+  
+    .extend({
+      
+      onModelSubscriptions: [],
+      
+      onModel: function(event, method){
+        this.onModelSubscriptions.push({
+          event: event,
+          method: method
+        })
+        return this
+      },
+      
+      onDOMSubscriptions: [],
+
+      onDOM: function(selector, event, method, argsMap){
+        this.onDOMSubscriptions.push({
+          event: event,
+          selector: selector,
+          method: method,
+          argsMap: argsMap
+        })
+        return this
+      }
+      
+    })
+  
+})
---
+++ test/tests/view.js
@@ -0,0 +1,71 @@
+(function($){
+
+  function appendDivToBody(){
+    return $('<div>')
+      .attr({id: 'test-div'})
+      .append(
+        $('<div>').addClass('inner')
+      )
+      .appendTo($('body'))
+  }
+
+  describe("views", function(){
+  
+    var model, $div
+  
+    beforeEach(function(){
+      model = Model.create()
+      $div = appendDivToBody()
+    })
+  
+    it("should allow initalizing with a selector", function(){
+      var view = View.create('#test-div')
+      expect(view.elem.id).toEqual('test-div')
+    })
+  
+    it("should allow initalizing with a DOM element", function(){
+      var view = View.create($('#test-div')[0])
+      expect(view.elem.id).toEqual('test-div')
+    })
+  
+    it("should allow initalizing with a jQuery element", function(){
+      var view = View.create($('#test-div'))
+      expect(view.elem.id).toEqual('test-div')
+    })
+  
+    it("should bind to model events", function(){
+      var render = jasmine.createSpy('render')
+      View.onModel('sneeze', 'render')
+          .include({render: render})
+      var view = View.create('#test-div', model)
+      model.emit('sneeze', [7])
+      expect(render).toHaveBeenCalledWith(model, 7)
+    })  
+
+    it("should bind to DOM events", function(){
+      model.fart = jasmine.createSpy('fart')
+      View.onDOM('.inner', 'click', 'fart')
+      var view = View.create('#test-div', model)
+      $('#test-div .inner').click()
+      expect(model.fart).toHaveBeenCalled()
+    })
+
+    it("should allow mapping the args", function(){
+      model.fart = jasmine.createSpy('fart')
+      View.onDOM('.inner', 'click', 'fart', function(evt){ return [evt.type, 'beetle'] })
+      var view = View.create('#test-div', model)
+      $('#test-div .inner').click()
+      expect(model.fart).toHaveBeenCalledWith('click', 'beetle')
+    })
+    
+    it("should allow returning a non-array for the args mapper", function(){
+      model.fart = jasmine.createSpy('fart')
+      View.onDOM('.inner', 'click', 'fart', function(evt){ return evt.type + 'beetle' })
+      var view = View.create('#test-div', model)
+      $('#test-div .inner').click()
+      expect(model.fart).toHaveBeenCalledWith('clickbeetle')
+    })
+  
+  })
+  
+})(jQuery)
